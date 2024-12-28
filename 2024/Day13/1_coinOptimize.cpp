65b25e149a04967815e9786e254ff6c1cd92dcb2
RabbitArch <xiali123.ch@gmail.com>
2024-12-28 23:30:02 +0800

    Learning regex.

---
+++ 2024/Day13/1_coinOptimize.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+#define BUFFER_SIZE 1024
+using namespace std;
+typedef pair<int, int> button;
+typedef pair<int, int> prize;
+int main() {
+    FILE* input = fopen( "input.txt", "r" );
+    char buf[BUFFER_SIZE + 1] = "\0";
+    int lineCnt = 0;
+    button a, b;
+    prize p;
+    while( !feof( input ) && fgets( buf, BUFFER_SIZE + 1, input ) ) {
+        
+    }
+}
