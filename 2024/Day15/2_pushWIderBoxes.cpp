a9a7a372d1ce500b9ea9018d6362953403c76732
RabbitArch <xiali123.ch@gmail.com>
2025-01-01 14:39:12 +0800

    Ehhhh.

---
+++ 2024/Day15/2_pushWIderBoxes.cpp
@@ -0,0 +1,250 @@
+#include "box.h"
+int opId = 0;
+vector<boxPos> getNextBoxVertical( vector<boxPos> const id2BoxPos, vector<vector<int>> const acrade, vector<boxPos> const curLevel, char const control, bool& isBlocked ) {
+    map<int, int> nextLevelID;
+    vector<boxPos> nextLevel;
+    for( int i = 0; i < curLevel.size() && !isBlocked; i++ ) {
+        boxPos curBox = curLevel[i];
+        {
+            pos l = getNextPos( control, curBox.l ), r = getNextPos( control, curBox.r );
+            int nextIDL = acrade[l.x][l.y], nextIDR = acrade[r.x][r.y];
+            if( nextIDL == WALL || nextIDR == WALL ) {
+                isBlocked = true;
+                nextLevel.clear();
+                // Unable to move.
+                return nextLevel;
+            }
+            if( nextIDL != EMPTY )
+                nextLevelID[nextIDL]++;
+            if( nextIDR != EMPTY )
+                nextLevelID[nextIDR]++;
+        }
+    }
+    for( auto m : nextLevelID ) {
+        nextLevel.push_back( id2BoxPos[m.first] );
+    }
+    return nextLevel;
+}
+
+vector<boxPos> getNextBoxHorizental( vector<boxPos> const id2BoxPos, vector<vector<int>> const  acrade, vector<boxPos> const curLevel, char const control, bool& isBlocked ) {
+    assert( curLevel.size() == 1 );
+    vector<boxPos> nextLevel;
+    boxPos curBox = curLevel[0];
+    pos nextBoxPos;
+    if( control == '<' ) {
+        nextBoxPos = getNextPos( control, curBox.l );
+    }
+    else {
+        nextBoxPos = getNextPos( control, curBox.r );
+    }
+    if( isBox( acrade, nextBoxPos ) ) {
+        nextLevel.push_back( getBoxPosAtPos( id2BoxPos, acrade, nextBoxPos ) );
+
+    }
+    else if( isWALL( acrade, nextBoxPos ) ) {
+        isBlocked = true;
+
+    }
+    else {
+        assert( nextLevel.size() == 0 );
+    }
+    return nextLevel;
+
+}
+
+void pushBoxVertical( vector<boxPos> const boxList, vector<boxPos>& id2BoxPos, vector<vector<int>>& acrade, char const control ) {
+    for( boxPos b : boxList ) {
+        pos l = b.l, r = b.r;
+        int boxId = acrade[l.x][l.y];
+        boxPos& curBox = id2BoxPos[boxId];
+        curBox.l = getNextPos( control, l );
+        curBox.r = getNextPos( control, r );
+        acrade[l.x][l.y] = EMPTY;
+        acrade[r.x][r.y] = EMPTY;
+        acrade[curBox.l.x][curBox.l.y] = boxId;
+        acrade[curBox.r.x][curBox.r.y] = boxId;
+    }
+}
+
+void pushBoxHorizontal( vector<boxPos>& boxList, vector<boxPos>& id2BoxPos, vector<vector<int>>& acrade, char const control ) {
+    assert( boxList.size() == 1 );
+    for( boxPos b : boxList ) {
+        pos l = b.l, r = b.r;
+        int boxId = acrade[l.x][l.y];
+        boxPos& curBox = id2BoxPos[boxId];
+        curBox.l = getNextPos( control, l );
+        curBox.r = getNextPos( control, r );
+        if( control == '>' )
+            acrade[l.x][l.y] = EMPTY;
+        else
+            acrade[r.x][r.y] = EMPTY;
+        acrade[curBox.l.x][curBox.l.y] = boxId;
+        acrade[curBox.r.x][curBox.r.y] = boxId;
+    }
+}
+
+void pushBox( vector<boxPos>& id2BoxPos, vector<vector<int>>& acrade, pos& curPos, char control ) {
+    // if( opId == 2579 )
+        // printGUI( acrade, curPos );
+    bool isBlocked = false;
+    stack<vector<boxPos>> boxPath;
+    vector<boxPos> curLevel;
+    // NextPos of start
+    pos nextPos = getNextPos( control, curPos );
+    curLevel.push_back( getBoxPosAtPos( id2BoxPos, acrade, nextPos ) );
+    boxPath.push( curLevel );
+    while( !isBlocked && !curLevel.empty() ) {
+        vector<boxPos> nextLevel;
+        if( control == 'v' || control == '^' ) {
+            nextLevel = getNextBoxVertical( id2BoxPos, acrade, curLevel, control, isBlocked );
+        }
+        else {
+            nextLevel = getNextBoxHorizental( id2BoxPos, acrade, curLevel, control, isBlocked );
+
+        }
+        if( !isBlocked ) {
+            if( !nextLevel.empty() )
+                boxPath.push( nextLevel );
+            curLevel = nextLevel;
+        }
+        else {
+            nextLevel.clear();
+            curLevel.clear();
+        }
+    }
+    if( !isBlocked ) {
+        while( !boxPath.empty() ) {
+            // if( opId == 2578 )
+                // printGUI( acrade, curPos );
+            vector<boxPos> preBoxList = boxPath.top();
+            boxPath.pop();
+            if( countWall( acrade ) != 758 ) {
+                printGUI( acrade, curPos );
+
+            }
+            if( control == '^' || control == 'v' ) {
+                pushBoxVertical( preBoxList, id2BoxPos, acrade, control );
+            }
+            else {
+                pushBoxHorizontal( preBoxList, id2BoxPos, acrade, control );
+            }
+            // if( opId == 2578 )
+                // printGUI( acrade, curPos );
+        }
+        curPos = nextPos;
+        // if( opId == 2578 )
+            // printGUI( acrade, curPos );
+    }
+}
+
+bool freshGUI( vector<boxPos>& id2BoxPos, vector<vector<int>>& acrade, pos& curPos ) {
+    char c = getMoveMent();
+    if( c == 'q' ) {  // 按 'q' 退出
+        return false;
+    }
+    // printGUI( acrade, curPos );
+    pos nextPos = getNextPos( c, curPos );
+    switch( acrade[nextPos.x][nextPos.y] ) {
+    case WALL:
+        break;
+    case EMPTY:
+        curPos = nextPos;
+        break;
+    default:
+        // Push boxes
+        pushBox( id2BoxPos, acrade, curPos, c );
+        break;
+    }
+    return true;
+}
+
+void play( vector<boxPos>& id2BoxPos, vector<vector<int>>& acrade, pos& curPos, queue<char> control ) {
+    // printGUI( acrade, curPos );
+    while( !control.empty() ) {
+        char c = control.front();
+        control.pop();
+        // printGUI( acrade, curPos );
+        pos nextPos = getNextPos( c, curPos );
+        switch( acrade[nextPos.x][nextPos.y] ) {
+        case WALL:
+            break;
+        case EMPTY:
+            curPos = nextPos;
+            break;
+        default:
+            // Push boxes
+            pushBox( id2BoxPos, acrade, curPos, c );
+            break;
+        }
+        opId++;
+        // printGUI( acrade, curPos );
+    }
+    // printGUI( acrade, curPos );
+}
+
+void playMyself( vector<boxPos>& id2BoxPos, vector<vector<int>>& acrade, pos& curPos ) {
+    printGUI( acrade, curPos );
+    enableRawMode(); // 开启字符缓冲模式
+    while( 1 ) {
+        if( freshGUI( id2BoxPos, acrade, curPos ) )
+            printGUI( acrade, curPos );
+        else return;
+    }
+    disableRawMode();
+}
+
+
+
+
+int main() {
+    vector<boxPos> id2BoxPos;
+    vector<vector<int>> acrade;
+    char buf[BUF] = "\0";
+    pos start;
+    FILE* input = fopen( "input.txt", "r" );
+    while( !feof( input ) && fgets( buf, BUF, input ) ) {
+        vector<int> row;
+        for( char c : buf ) {
+            if( c != '\n' && c != '\0' ) {
+                switch( c ) {
+                case '#':
+                    row.push_back( WALL );
+                    row.push_back( WALL );
+                    break;
+                case '.':
+                    row.push_back( EMPTY );
+                    row.push_back( EMPTY );
+                    break;
+                case 'O':
+                    row.push_back( id2BoxPos.size() );
+                    row.push_back( id2BoxPos.size() );
+                    id2BoxPos.push_back( boxPos( pos( acrade.size(), row.size() - 2 ), pos( acrade.size(), row.size() - 1 ) ) );
+                    break;
+                case '@':
+                    start = pos( acrade.size(), row.size() );
+                    row.push_back( EMPTY );
+                    row.push_back( EMPTY );
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        acrade.push_back( row );
+    }
+    FILE* move = fopen( "move.txt", "r" );
+    queue<char> control;
+    while( !feof( move ) && fgets( buf, BUF, move ) ) {
+        for( char c : buf ) {
+            if( c != '\n' && c != '\0' ) { control.push( c ); }
+        }
+    }
+    // 1472388
+    // cout << sumCoordinates( id2BoxPos, acrade ) << endl;
+    // printGUI( acrade, start );
+    // play( id2BoxPos, acrade, start, control );
+    playMyself( id2BoxPos, acrade, start );
+    printGUI( acrade, start );
+    cout << sumCoordinates( id2BoxPos, acrade ) << endl;
+    return 0;
+}
---
+++ 2024/Day15/box.h
@@ -0,0 +1,147 @@
+#include "bits/stdc++.h"
+#include <termios.h>
+using namespace std;
+#define WALL -1
+#define EMPTY -2
+#define BOT -3
+#define BUF 4096
+typedef struct pos {
+    pos() {};
+    pos( int a, int b ) : x( a ), y( b ) {}
+    int x, y;
+}pos;
+typedef struct boxPos {
+    boxPos() {}
+    boxPos( pos a, pos b ) : l( a ), r( b ) {}
+    pos l, r;
+}bos;
+typedef long long ll;
+void enableRawMode() {
+    termios term;
+    tcgetattr( STDIN_FILENO, &term );          // 获取当前终端设置
+    term.c_lflag &= ~( ICANON | ECHO );        // 关闭规范模式和回显
+    tcsetattr( STDIN_FILENO, TCSANOW, &term ); // 立即生效
+}
+
+void disableRawMode() {
+    termios term;
+    tcgetattr( STDIN_FILENO, &term );
+    term.c_lflag |= ( ICANON | ECHO );         // 恢复规范模式和回显
+    tcsetattr( STDIN_FILENO, TCSANOW, &term );
+}
+
+char getMoveMent() {
+    char c;
+    while( 1 ) {
+        c = getchar();
+        if( c == '\033' ) { // 检测转义序列（ESC 键）
+            getchar();     // 跳过 '['
+            switch( getchar() ) { // 读取方向键的第三个字符
+            case 'A': return '^';
+            case 'B': return 'v';
+            case 'C': return '>';
+            case 'D': return '<';
+            default: std::cout << "Unknown key" << std::endl; break;
+            }
+        }
+        else if( c == 27 ) { // ESC 键单独按下
+            return 0;
+        }
+        else {
+            // std::cout << "Normal key: " << c << std::endl;
+            continue;
+        }
+    }
+}
+
+void printGUI( vector<vector<int>> const acrade, pos const curPos ) {
+    system( "clear" );
+    vector<vector<int>> GUI = acrade;
+    GUI[curPos.x][curPos.y] = BOT;
+    for( auto line : GUI ) {
+        bool first = true;
+        for( auto id : line ) {
+            if( id >= 0 ) {
+                // cout << 'O';
+                if( first ) {
+                    first = false;
+                    cout << "[";
+                }
+                else {
+                    first = true;
+                    cout << "]";
+                }
+            }
+            else if( id == WALL ) {
+                cout << '#';
+            }
+            else if( id == EMPTY ) {
+                cout << ' ';
+            }
+            else {
+                cout << '@';
+            }
+        }
+        cout << endl;
+    }
+    return;
+}
+
+pos getNextPos( char const c, pos const curPos ) {
+    pos nextPos;
+    switch( c ) {
+    case '^':
+        nextPos = pos( curPos.x - 1, curPos.y );
+        break;
+    case '>':
+        nextPos = pos( curPos.x, curPos.y + 1 );
+        break;
+    case 'v':
+        nextPos = pos( curPos.x + 1, curPos.y );
+        break;
+    case '<':
+        nextPos = pos( curPos.x, curPos.y - 1 );
+        break;
+    default:
+        cout << "Error situation." << endl;
+        break;
+    }
+    return nextPos;
+}
+
+boxPos getBoxPosAtPos( vector<boxPos> const id2BoxPos, vector<vector<int>> const pos2Id, pos const curPos ) {
+    return id2BoxPos[pos2Id[curPos.x][curPos.y]];
+}
+
+bool isBox( vector<vector<int>> const pos2Id, pos point ) {
+    return pos2Id[point.x][point.y] >= 0;
+}
+bool isWALL( vector<vector<int>> const pos2Id, pos point ) {
+    return pos2Id[point.x][point.y] == WALL;
+}
+bool isEmpty( vector<vector<int>> const pos2Id, pos point ) {
+    return pos2Id[point.x][point.y] == EMPTY;
+}
+
+ll sumCoordinates( vector<boxPos> const id2BoxPos, vector<vector<int>> const acrade ) {
+    ll res = 0;
+    for( auto boxPos : id2BoxPos ) {
+        res += 100 * boxPos.l.x + boxPos.l.y;
+    }
+    return res;
+}
+
+int countWall( vector<vector<int>> const acrade ) {
+    int wallCnt = 0;
+    for( int i = 0; i < acrade.size(); i++ ) {
+        for( int j = 0; j < acrade[i].size(); j++ ) {
+            if( acrade[i][j] == WALL ) {
+                // cout << "Pos" << i << "," << j << endl;
+                wallCnt++;
+            }
+
+        }
+
+    }
+    return wallCnt;
+}
