9beae45e9711855027a2624b8de341250c4d04ef
RabbitArch <xiali123.ch@gmail.com>
2025-01-26 22:27:05 +0800

    Submit Day04 2023.

---
+++ 2023Java/Day04/CardPoints.java
@@ -0,0 +1,85 @@
+package Day04;
+
+import java.io.*;
+import java.util.*;
+
+public class CardPoints {
+    HashSet<Integer> winningNumbers;
+
+    void Solution1() throws IOException {
+        Integer res = 0;
+        BufferedReader input = new BufferedReader(new FileReader("input.txt"));
+        String buf;
+        while ((buf = input.readLine()) != null) {
+            winningNumbers = new HashSet<>();
+
+            String[] cards = buf.split(":")[1].split("\\|");
+            for (String split : cards[0].trim().split("\\s+")) {
+                winningNumbers.add(Integer.parseInt(split));
+            }
+            Integer point = 0;
+            for (String split : cards[1].trim().split("\\s+")) {
+                Integer holdingCard = Integer.parseInt(split);
+                if (winningNumbers.contains(holdingCard)) {
+                    if (point.equals(0)) {
+                        point = 1;
+                    } else {
+                        point *= 2;
+                    }
+                }
+            }
+            res += point;
+        }
+        System.out.println("Solution 1: " + res);
+        input.close();
+    }
+
+    void Solution2() throws IOException {
+        Integer res = 0, currentBonus, currentLine = 0;
+        ArrayList<Integer> bonusRecord = new ArrayList<>();
+        BufferedReader input = new BufferedReader(new FileReader("input.txt"));
+        String buf;
+        while ((buf = input.readLine()) != null) {
+            if (currentLine == bonusRecord.size()) {
+                //The original one.
+                bonusRecord.add(1);
+            }
+            currentBonus = bonusRecord.get(currentLine);
+            res += currentBonus;
+
+            winningNumbers = new HashSet<>();
+
+            String[] cards = buf.split(":")[1].split("\\|");
+            for (String split : cards[0].trim().split("\\s+")) {
+                winningNumbers.add(Integer.parseInt(split));
+            }
+
+            Integer bonus = 0;
+
+            for (String split : cards[1].trim().split("\\s+")) {
+                Integer holdingCard = Integer.parseInt(split);
+                if (winningNumbers.contains(holdingCard)) {
+                    bonus++;
+                }
+            }
+
+            for (int i = 1; i <= bonus; i++) {
+                if (currentLine + i < bonusRecord.size()) {
+                    bonusRecord.set(currentLine + i, bonusRecord.get(currentLine + i) + currentBonus);
+                } else {
+                    assert (currentLine + i == bonusRecord.size());
+                    bonusRecord.add(currentBonus + 1);
+                }
+            }
+            currentLine++;
+        }
+        System.out.println("Solution 2: " + res);
+        input.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        CardPoints Solution = new CardPoints();
+        Solution.Solution1();
+        Solution.Solution2();
+    }
+}
