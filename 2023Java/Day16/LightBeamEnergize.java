1fe7cffc9848b42a0b34113b044cfd456a1d75f0
RabbitArch <xiali123.ch@gmail.com>
2025-03-05 01:11:43 +0800

    Day16 p2.

--- 2023Java/Day16/LightBeamEnergize.java
+++ 2023Java/Day16/LightBeamEnergize.java
@@ -2,7 +2,6 @@ package Day16;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 import JavaDataModel.Pair;
 import JavaDataModel.Point2D;
 
@@ -14,25 +13,29 @@ public class LightBeamEnergize {
         BufferedReader input = new BufferedReader(new FileReader("input.txt"));
         String buf;
         while ((buf = input.readLine()) != null) {
-            // List<Character> row = IntStream.range(0, buf.length()).mapToObj(i ->
+            // List<Character> row = IntStream.range(0, buf.length()).mapToObj(i
+            // ->
             // buf.charAt(i)).toList();
-            ContraptionLayout.add(buf.trim().chars().mapToObj(c -> (char) c).toList());
+            ContraptionLayout
+                    .add(buf.trim().chars().mapToObj(c -> (char) c).toList());
         }
         input.close();
     }
 
-    HashMap<Character, HashMap<Integer, Integer>> nextDirection =
-            new HashMap<>(Map.of('/', new HashMap<>(Map.of(0, 3, 1, 2, 2, 1, 3, 0)), '\\',
+    HashMap<Character, HashMap<Integer, Integer>> nextDirection = new HashMap<>(
+            Map.of('/', new HashMap<>(Map.of(0, 3, 1, 2, 2, 1, 3, 0)), '\\',
                     new HashMap<>(Map.of(0, 2, 1, 3, 2, 0, 3, 1))));
 
-    void Solution1() throws IOException {
-        readFile();
-        int rows = ContraptionLayout.size(), cols = ContraptionLayout.get(0).size();
+    int energizeTiles(Point2D entry, int face) {
+        if (memo.containsKey(new Pair<>(entry, face))) {
+            return memo.get(new Pair<>(entry, face));
+        }
         HashSet<Point2D> resSet = new HashSet<>();
-        Point2D entry = new Point2D(0, 0);
+        int rows = ContraptionLayout.size(),
+                cols = ContraptionLayout.get(0).size();
         Queue<Pair<Point2D, Integer>> q = new LinkedList<>();
         HashSet<Pair<Point2D, Integer>> visited = new HashSet<>();
-        q.add(new Pair<>(entry, 3));
+        q.add(new Pair<>(entry, face));
         while (!q.isEmpty()) {
             Pair<Point2D, Integer> curLight = q.poll();
             if (visited.contains(curLight))
@@ -40,16 +43,18 @@ public class LightBeamEnergize {
             resSet.add(curLight.first);
             visited.add(curLight);
             // printContraptionMap(resSet);
-            char curTile =
-                    ContraptionLayout.get(curLight.first.getKey()).get(curLight.first.getValue());
+            char curTile = ContraptionLayout.get(curLight.first.first)
+                    .get(curLight.first.second);
             if (curTile == '.' || (curTile == '|' && curLight.second < 2)
                     || (curTile == '-' && curLight.second > 1)) {
-                Point2D nextPos = Point2D.getNextPosition(curLight.first, curLight.second);
+                Point2D nextPos = Point2D.getNextPosition(curLight.first,
+                        curLight.second);
                 if (Point2D.isValid(rows, cols, nextPos))
                     q.add(new Pair<Point2D, Integer>(nextPos, curLight.second));
             } else if (nextDirection.containsKey(curTile)) {
                 int nextDir = nextDirection.get(curTile).get(curLight.second);
-                Point2D nextPos = Point2D.getNextPosition(curLight.first, nextDir);
+                Point2D nextPos =
+                        Point2D.getNextPosition(curLight.first, nextDir);
                 if (Point2D.isValid(rows, cols, nextPos))
                     q.add(new Pair<Point2D, Integer>(nextPos, nextDir));
             } else {
@@ -57,25 +62,54 @@ public class LightBeamEnergize {
                 if (curLight.second >= 2) {
                     nextDir = 0;
                 }
-                Point2D nextPos1 = Point2D.getNextPosition(curLight.first, nextDir);
-                if (Point2D.isValid(rows, cols, nextPos1))
-                    q.add(new Pair<>(nextPos1, nextDir));
-                Point2D nextPos2 = Point2D.getNextPosition(curLight.first, nextDir + 1);
-                if (Point2D.isValid(rows, cols, nextPos2))
-                    q.add(new Pair<>(nextPos2, nextDir + 1));
+                for (int i = 0; i < 2; i++) {
+                    Point2D nextPos = Point2D.getNextPosition(curLight.first,
+                            nextDir + i);
+                    if (Point2D.isValid(rows, cols, nextPos))
+                        q.add(new Pair<>(nextPos, nextDir + i));
+                }
             }
         }
-
-        System.out.println("Solution 1: " + resSet.size());
+        // printContraptionMap(resSet);
+        return resSet.size();
     }
 
+    void Solution1() throws IOException {
+        readFile();
+        System.out
+                .println("Solution 1: " + energizeTiles(new Point2D(0, 0), 3));
+    }
+
+    HashMap<Pair<Point2D, Integer>, Integer> memo = new HashMap<>();
+
     void Solution2() {
-        int maxTiles = 0;
+        int maxEnergizedTiles = 0;
+        // Point2D targetEntry = null;
+        int rows = ContraptionLayout.size(),
+                cols = ContraptionLayout.get(0).size();
+        for (int j = 0; j < cols; j++) {
+            maxEnergizedTiles = Math.max(energizeTiles(new Point2D(0, j), 1),
+                    maxEnergizedTiles);
+            maxEnergizedTiles =
+                    Math.max(energizeTiles(new Point2D(rows - 1, j), 0),
+                            maxEnergizedTiles);
+            // memo.put(new Pair<>(curEntry, 1), curEnergized);
+        }
+        for (int i = 0; i < rows; i++) {
+            maxEnergizedTiles = Math.max(energizeTiles(new Point2D(i, 0), 2),
+                    maxEnergizedTiles);
+            maxEnergizedTiles =
+                    Math.max(energizeTiles(new Point2D(i, cols - 1), 3),
+                            maxEnergizedTiles);
+        }
+        System.out.println("Solution 2: " + maxEnergizedTiles);
     }
 
     void printContraptionMap(HashSet<Point2D> resSet) {
+        System.out.println();
         List<List<Character>> canvas = ContraptionLayout.stream()
-                .map(list -> (List<Character>) (new ArrayList<>(list))).toList();
+                .map(list -> (List<Character>) (new ArrayList<>(list)))
+                .toList();
         for (var p : resSet)
             canvas.get(p.first).set(p.second, '#');
         canvas.forEach(l -> {
@@ -87,5 +121,6 @@ public class LightBeamEnergize {
     public static void main(String[] args) throws IOException {
         LightBeamEnergize Solution = new LightBeamEnergize();
         Solution.Solution1();
+        Solution.Solution2();
     }
 }
