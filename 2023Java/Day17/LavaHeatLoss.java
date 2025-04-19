80cbd5a1d33be53603c18da70010cf2c1e077750
RabbitXWindows <xiali123.ch@gmail.com>
2025-04-19 18:45:03 +0800

    Clean workspace.

--- 2023Java/Day17/LavaHeatLoss.java
+++ 2023Java/Day17/LavaHeatLoss.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import JavaDataModel.Point2D;
 import JavaDataModel.Pair;
