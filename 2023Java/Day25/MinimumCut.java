3081b723006288834fe3e4a391b4d67b3f720683
RabbitXWindows <xiali123.ch@gmail.com>
2025-05-18 20:19:15 +0800

    Add timeout mechanism.

--- 2023Java/Day25/MinimumCut.java
+++ 2023Java/Day25/MinimumCut.java
@@ -8,23 +8,28 @@ public class MinimumCut {
         BufferedReader input = new BufferedReader(new FileReader("input.txt"));
         input.lines().forEach(l -> {
             var split = l.split(":");
-            String inWire = split[0].trim();
+            int inWire = Hasher(split[0].trim());
             Arrays.stream(split[1].trim().split(" ")).forEach(w -> {
-                WireGraph.computeIfAbsent(inWire, s -> new HashSet<>()).add(w);
-                WireGraph.computeIfAbsent(w, s -> new HashSet<>()).add(inWire);
+                int wHash = Hasher(w);
+                WireGraph.computeIfAbsent(inWire, s -> new HashSet<>()).add(wHash);
+                WireGraph.computeIfAbsent(wHash, s -> new HashSet<>()).add(inWire);
             });
         });
         input.close();
     }
 
-    Map<String, Set<String>> WireGraph = new HashMap<>();
+    int Hasher(String WireName) {
+        return WireName.chars().reduce(0, (i, c) -> i * 26 + c - 'a');
+    }
 
-    int CutCount(Map<String, Set<String>> Cliques) {
+    Map<Integer, Set<Integer>> WireGraph = new HashMap<>();
+
+    int CutCount(Map<Integer, Set<Integer>> Cliques) {
         if (Cliques.size() != 2)
             return -1;
-        List<String> CliqueNames = Cliques.keySet().stream().toList();
+        List<Integer> CliqueNames = Cliques.keySet().stream().toList();
         int cutCnt = 0;
-        String from = CliqueNames.get(0), to = CliqueNames.get(1);
+        int from = CliqueNames.get(0), to = CliqueNames.get(1);
         for (var fromV : Cliques.get(from)) {
             for (var conn : WireGraph.get(fromV)) {
                 if (Cliques.get(to).contains(conn)) {
@@ -35,57 +40,67 @@ public class MinimumCut {
         return cutCnt;
     }
 
-    String FindFather(Map<String, String> Hierachy, String name) {
+    int FindFather(Map<Integer, Integer> Hierachy, int name) {
         if (Hierachy.get(name).equals(name)) {
             return name;
         }
         return Hierachy.compute(name, (k, e) -> FindFather(Hierachy, Hierachy.get(e)));
     }
 
-    void Solution1() throws Exception {
-        readFile();
-        Random rand = new Random(System.nanoTime());
-        // unionset - k,v : parent, clique-member(Include parent)
-        Map<String, Set<String>> Cliques = new HashMap<>();
-        while (CutCount(Cliques) != 3) {
-            Cliques.clear();
-            Map<String, String> Father = new HashMap<>();
-            Set<String> NodesHavingEdge = new HashSet<>(WireGraph.keySet());// Returns a Set view of the keys contained
-                                                                            // in this map.
-            // The set is backed by the map, so changes to the map are reflected in the set,
-            // and vice-versa.
-            HashMap<String, Set<String>> visited = new HashMap<>();
-            WireGraph.keySet().forEach(e -> {
-                visited.put(e, new HashSet<>());
-                Cliques.put(e, new HashSet<>(Set.of(e)));
-                Father.put(e, e);
-            });
-            // while (!NodesHavingEdge.isEmpty() && CutCount(Cliques) != 3) { // This cost over 3 minutes
-            while (Cliques.size() != 2) { // 300 ms - 2 minutes
-                // Get father
-                List<String> NodeList = NodesHavingEdge.stream().toList();
-                String father = NodeList.get(rand.nextInt(NodeList.size()));
-                // Get child
-                List<String> ChildList = WireGraph.get(father).stream()
-                        .filter(child -> !visited.get(father).contains(child) && !visited.get(child).contains(father))
-                        .toList();
-                if (ChildList.isEmpty()) {
-                    NodesHavingEdge.remove(father);
-                    continue;
-                }
-                String child = ChildList.get(rand.nextInt(ChildList.size()));
-                visited.get(father).add(child);
-                visited.get(child).add(father);
-                String FatherCliqueName = FindFather(Father, father);
-                String ChildCliqueName = FindFather(Father, child);
-                Father.put(ChildCliqueName, FatherCliqueName);
-                if (!FatherCliqueName.equals(ChildCliqueName)) {
-                    Cliques.get(FatherCliqueName).addAll(Cliques.get(ChildCliqueName));
-                    Cliques.remove(ChildCliqueName);
-                }
+    Map<Integer, Set<Integer>> Karger(long seed, long timeout) {
+        var now = System.currentTimeMillis();
+        Random rand = new Random(seed);
+        Map<Integer, Set<Integer>> Cliques = new HashMap<>();
+        Map<Integer, Integer> Hierachy = new HashMap<>();
+        Set<Integer> NodesHavingEdge = new HashSet<>(WireGraph.keySet());
+        HashMap<Integer, Set<Integer>> visited = new HashMap<>();
+        WireGraph.keySet().forEach(e -> {
+            visited.put(e, new HashSet<>());
+            Cliques.put(e, new HashSet<>(Set.of(e)));
+            Hierachy.put(e, e);
+        });
+        while (Cliques.size() != 2) {
+            if (System.currentTimeMillis() - now > timeout) {
+                break;
+            }
+            // Get father
+            List<Integer> NodeList = NodesHavingEdge.stream().toList();
+            Integer father = NodeList.get(rand.nextInt(NodeList.size()));
+            // Get child
+            List<Integer> ChildList = WireGraph.get(father).stream()
+                    .filter(child -> !visited.get(father).contains(child) && !visited.get(child).contains(father))
+                    .toList();
+            if (ChildList.isEmpty()) {
+                NodesHavingEdge.remove(father);
+                continue;
+            }
+            Integer child = ChildList.get(rand.nextInt(ChildList.size()));
+            visited.get(father).add(child);
+            visited.get(child).add(father);
+            int FatherCliqueName = FindFather(Hierachy, father), ChildCliqueName = FindFather(Hierachy, child);
+            Hierachy.put(ChildCliqueName, FatherCliqueName);
+            if (FatherCliqueName != ChildCliqueName) {
+                Cliques.get(FatherCliqueName).addAll(Cliques.get(ChildCliqueName));
+                Cliques.remove(ChildCliqueName);
             }
         }
-        System.out.println("Solution 1: " + Cliques.values().stream().mapToInt(s -> s.size()).reduce(1, (i, v) -> i * v));
+        return Cliques;
+    }
+
+    void Solution1() throws Exception {
+        readFile();
+        long timeout = 70;
+        while (true) {
+            long seed = System.currentTimeMillis();
+            var res = Karger(seed, timeout);
+            if (CutCount(res) == 3) {
+                System.out.println("Using seed: " + seed);
+                System.out.println("Solution 1: " + res.values().stream().mapToInt(s -> s.size()).reduce(1, (i, v) -> i * v));
+                break;
+            }
+        }
+        // 1747570281531 - 40ms
+        System.out.println("Solution 1: " + Karger(1747570281531L,Long.MAX_VALUE).values().stream().mapToInt(s -> s.size()).reduce(1, (i, v) -> i * v));
     }
 
     void Solution2() throws Exception {
