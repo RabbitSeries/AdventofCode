24ea62804de4643cea58e407e129e629b256a09c
RabbitXWindows <xiali123.ch@gmail.com>
2025-07-29 23:43:31 +0800

    chore: clean

--- Python/2022/main/year2022/Day19/NotEnoughMinerals.py
+++ Python/2022/main/year2022/Day19/NotEnoughMinerals.py
@@ -1,23 +1,17 @@
 import re
-from typing import Literal
 import numpy as np
 
 
 class BluePrint:
     robots = ["ore", "clay", "obsidian", "geode"]
 
-    def __init__(self, robotDatas: list[str]):
-        self.costs = np.zeros((4, 4), dtype=int)
-        for cost, robotData in zip(self.costs, robotDatas):
-            for j, mineral in enumerate(self.robots):
-                pattern = re.compile(Rf"(\d+)(?: {mineral})")
-                res = next(pattern.finditer(robotData), None)
-                cost[j] = 0 if res is None else int(res.group(1))
+    def __init__(self, data: list[str]):
+        self.costs = np.array([[int(m.group(1)) if (m := re.search(fr"(\d+) {r_type}", d)) else 0 for r_type in self.robots]
+                               for d in data])
 
     def __repr__(self) -> str:
-        return ". ".join([f"Each {robot} robot costs "
-                          + ' and '.join([f'{n} {name}' for n, name in zip(cost, self.robots) if n != 0])
-                          for robot, cost in zip(self.robots, self.costs)]) + "."
+        return ". ".join([f"Each {rbt} robot costs " + ' and '.join([f'{c} {rbt}' for c in cost if c != 0])
+                          for rbt, cost in zip(self.robots, self.costs)]) + "."
 
 
 class NotEnoughMinerals:
@@ -26,7 +20,7 @@ class NotEnoughMinerals:
             self.bluePrints = [BluePrint(line.split(":")[1].split(".")) for line in f.read().splitlines()]
 
     def __repr__(self) -> str:
-        return "\n".join([f"Blueprint {i}: {bluePrint}" for i, bluePrint in enumerate(self.bluePrints, 1)])
+        return "\n".join([f"Blueprint {i}: {bp}" for i, bp in enumerate(self.bluePrints, 1)])
 
 
 if __name__ == "__main__":
