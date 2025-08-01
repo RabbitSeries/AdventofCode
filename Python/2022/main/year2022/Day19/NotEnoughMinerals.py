83e92734c1cba69e604777f89666f7b332e946ec
RabbitXWindows <xiali123.ch@gmail.com>
2025-08-01 19:37:04 +0800

    chore: I hate python

--- Python/2022/main/year2022/Day19/NotEnoughMinerals.py
+++ Python/2022/main/year2022/Day19/NotEnoughMinerals.py
@@ -1,7 +1,10 @@
-from functools import lru_cache
+from collections import deque
+from functools import cache
 import re
 from aoc_libs.utils.ISolution import ISolution
 import math
+import numpy as np
+from queue import PriorityQueue
 
 
 class BluePrint:
@@ -10,6 +13,7 @@ class BluePrint:
     def __init__(self, data: str):
         self.prototypes = tuple(tuple(int(m.group(1)) if (m := re.search(fr"(\d+) {r_type}", d)) else 0 for r_type in self.robots)
                                 for d in data.split(":")[1].strip().split(".") if d)
+        self.max = tuple(map(int, np.max(self.prototypes, axis=0)))
 
     def __repr__(self) -> str:
         return ". ".join([f"Each {rbt} robot costs " + ' and '.join([f'{c} {rbt}' for c in cost if c != 0])
@@ -32,37 +36,41 @@ class NotEnoughMinerals(ISolution):
     def __repr__(self) -> str:
         return "\n".join([f"Blueprint {i}: {bp}" for i, bp in enumerate(self.bluePrints, 1)])
 
-    @lru_cache(None)
+    @cache
     def DigDigDig(self, countDown: int, blp: BluePrint,
                   rbt: tuple[int, ...] = (1, 0, 0, 0),
                   minerals: tuple[int, ...] = (0, 0, 0, 0)) -> int:
         if countDown <= 0:
             return minerals[-1]
-        maxGeode = minerals[-1]
-        for i, cost in enumerate(blp.prototypes):
-            delta = [max(0, r-m) for r, m in zip(cost, minerals)]
-            if any(d != 0 and r == 0 for d, r in zip(delta, rbt)):
-                maxGeode = max(maxGeode, minerals[-1] + rbt[-1]*countDown)
+        maxGeode = minerals[-1] + rbt[-1] * countDown
+        for i in reversed(range(4)):
+            cost = blp.prototypes[i]
+            if i != 3 and rbt[i] >= blp.max[i]:
                 continue
-            time = max(max(0, 0 if d == 0 else math.ceil(d/r)) for d, r in zip(delta, rbt))+1
+            delta = [max(0, c-m) for c, m in zip(cost, minerals)]
+            if any(d != 0 and r == 0 for d, r in zip(delta, rbt)):
+                continue
             # time required to build the next robot
+            time = max(0 if d == 0 else math.ceil(d/r) for d, r in zip(delta, rbt)) + 1
             if countDown > time:
-                nextRobots = tuple([*rbt[0:i], rbt[i]+1, *rbt[i+1:]])
-                nextMinerals = tuple(r*time+m-c for r, m, c in zip(rbt, minerals, cost))
+                nextRobots = tuple(r + (1 if j == i else 0) for j, r in enumerate(rbt))
+                nextMinerals = tuple(min(r*time+m-c, blpm*(countDown-time)) if blpm != 0 else r*time+m-c
+                                     for r, m, c, blpm in zip(rbt, minerals, cost, blp.max))  # Increase cache hit rate
                 maxGeode = max(maxGeode, self.DigDigDig(countDown-time, blp, nextRobots, nextMinerals))
-            else:
-                maxGeode = max(maxGeode, minerals[-1] + rbt[-1]*countDown)
         return maxGeode
 
-    def Part1(self):
+    def solve(self, countDown: int, last: int | None = None):
         res = []
-        for i, blp in enumerate(self.bluePrints, 1):
-            res.append((i, self.DigDigDig(24, blp)))
+        for i, blp in enumerate(self.bluePrints[0:last], 1):
+            res.append((i, self.DigDigDig(countDown, blp)))
             print(res)
-        print(f'Part 1: {sum([i*c for i, c in res])}')
+        return res
+
+    def Part1(self):
+        print(f'Part 1: {sum(a*b for a, b in self.solve(24))}')
 
     def Part2(self):
-        pass
+        print(f'Part 2: {math.prod(b for _, b in self.solve(32, 3))}')
 
     def test(self):
         data = '''Blueprint 1:
@@ -87,4 +95,4 @@ if __name__ == "__main__":
     # solution.test()
     # This is so f**king slow
     solution.Part1()
-    # solution.Part2()
+    solution.Part2()
