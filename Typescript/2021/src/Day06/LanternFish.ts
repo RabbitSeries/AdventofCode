00b914e712168c15c386f2fff516c42e4fcc5fb9
RabbitXWindows <xiali123.ch@gmail.com>
2026-07-19 22:32:38 +0800

    2021.day14.part2

--- Typescript/2021/src/Day06/LanternFish.ts
+++ Typescript/2021/src/Day06/LanternFish.ts
@@ -1,6 +1,7 @@
-import { readFile } from "node:fs/promises";
+import { readFile } from 'node:fs/promises'
 
-const ages = await readFile("Day06/input.txt").then(line => line.toString().trim().split(",").map(age => parseInt(age)))
+const ages = await readFile('Day06/input.txt')
+    .then(line => line.toString().trim().split(',').map(age => parseInt(age)))
 
 const lanternFishes: Map<number, number> = new Map()
 
@@ -15,7 +16,8 @@ function propagation(lanternFishes: Map<number, number>, days: number) {
         for (const [k, v] of fishes.entries()) {
             if (k) {
                 lanternFishes.set(k - 1, (lanternFishes.get(k - 1) ?? 0) + v)
-            } else {
+            }
+            else {
                 lanternFishes.set(6, (lanternFishes.get(6) ?? 0) + v)
                 lanternFishes.set(8, (lanternFishes.get(8) ?? 0) + v)
             }
--- Typescript/2021/src/Day14/ExtendedPolymerization.ts
+++ Typescript/2021/src/Day14/ExtendedPolymerization.ts
@@ -4,7 +4,12 @@ import { EOL } from 'node:os'
 export async function main() {
     const data = await readFile('Day14/input.txt', 'utf8')
     const blocks = data.split(`${EOL}${EOL}`)
-    let polymer = [...blocks[0]]
+    let polymers = new Map<string, number>()
+    for (let i = 0; i <= blocks[0].length; i++) {
+        const zipWithNext = (i ? blocks[0][i - 1] : ' ')
+          + (blocks[0].length - i ? blocks[0][i] : ' ')
+        polymers.set(zipWithNext, polymers.getOrInsert(zipWithNext, 0) + 1)
+    }
     const rules = blocks[1].split(`${EOL}`).reduce((ruleMap, rule) => {
         const m = rule.match(/(\w\w) -> (\w)/)
         if (m) {
@@ -12,26 +17,42 @@ export async function main() {
         }
         return ruleMap
     }, new Map<string, string>())
-    for (let i = 0; i < 10; i++) {
-        const processed: string[] = []
-        for (let j = 1; j < polymer.length; j++) {
-            const key = polymer[j - 1] + polymer[j]
-            if (rules.has(key)) {
-                processed.push(polymer[j - 1], rules.get(key)!)
+    function getStat() {
+        const count = Array(26).fill(0)
+        for (const [pair, pairN] of polymers) {
+            if (pair[0] !== ' ') {
+                count[pair.charCodeAt(0) - 'A'.charCodeAt(0)] += pairN
             }
-            else {
-                processed.push(polymer[j - 1])
+            if (pair[1] !== ' ') {
+                count[pair.charCodeAt(1) - 'A'.charCodeAt(0)] += pairN
             }
         }
-        processed.push(polymer[polymer.length - 1])
-        polymer = processed
+        count.sort((a, b) => b - a)
+        // Each one is counted twice
+        // This can be proved using Mathematical Induction.
+        return count[0] / 2 - count[count.findLastIndex(v => v !== 0)] / 2
     }
-    const count = Array(26).fill(0)
-    for (const ch of polymer) {
-        count[ch.charCodeAt(0) - 'A'.charCodeAt(0)]++
+    for (let i = 0; i < 40; i++) {
+        if (i === 10) {
+            console.log(getStat())
+        }
+        const newPolymers: typeof polymers = new Map()
+        for (const [pair, pairN] of polymers) {
+            if (rules.has(pair)) {
+                const pair1 = pair[0] + rules.get(pair)!
+                const pair2 = rules.get(pair)! + pair[1]
+                newPolymers.set(pair1,
+                    newPolymers.getOrInsert(pair1, 0) + pairN)
+                newPolymers.set(pair2,
+                    newPolymers.getOrInsert(pair2, 0) + pairN)
+            }
+            else {
+                newPolymers.set(pair, pairN)
+            }
+        }
+        polymers = newPolymers
     }
-    count.sort((a, b) => b - a)
-    console.log(count[0] - count[count.findLastIndex(v => v !== 0)])
+    console.log(getStat())
 }
 
 await main()
